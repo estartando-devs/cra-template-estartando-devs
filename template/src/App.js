c674394af0eb27cbf9f543252801fd99628293b5
Alan Araujo <alan@estartandodevs.com.br>
2021-07-28 21:34:37 -0300

    Fix imports

--- template/src/App.js
+++ template/src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Routes from "./routes";
-import { GlobalStyles } from "./styles/globalStyled";
-import theme from "./styles/Theme";
-import { store, persistor } from "./store";
-import { PersistGate } from "redux-persist/integration/react";
-import { Provider } from "react-redux";
-import { ThemeProvider } from "styled-components";
+
+import "./styles/global.scss";
+import "./styles/reset.scss";
+import "./styles/fonts.scss";
 
 export default function App() {
   return (
--- template/src/routes.js
+++ template/src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import {
   Home
-} from './containers';
+} from './pages';
 
 export default function Routes() {
   return (
